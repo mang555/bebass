044c9dcfa3600b6ef03ab8edfb7f9f3538cea093
mang555 <akmal.fahrian346@gmail.com>
2022-02-14 08:30:06 +0700

    cardview

--- app/src/main/java/com/example/latihan/BebasActivity.java
+++ app/src/main/java/com/example/latihan/BebasActivity.java
@@ -2,18 +2,13 @@ package com.example.latihan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
-
+public class BebasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-       
-
+        setContentView(R.layout.activity_bebas);
     }
 }
